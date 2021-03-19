4071f301bac6268ab4caf55b3bff172c4fc6ed38
JESSICA ADRIANA FEITOSA <60003548@midway.com.br>
2021-03-19 07:33:12 -0300

    Atividade de recursão 18032021

---
+++ src/main/java/Semestre_4_P/O/T/A/Atividade_18032021.java
@@ -0,0 +1,31 @@
+package Semestre_4_P.O.T.A;
+
+public class Atividade_18032021 {
+
+	public static void main(String[] args) {
+		//metodo(1024);
+		
+		System.out.println(metodo1(5));
+
+	}
+	
+	public static int metodo1(int n) {
+		int c= 0;
+		for (int i =0; i <= n; i++) {
+			c = c + 1;
+			System.out.println("v" + i);
+		}
+		return c;
+	}
+	
+	public static int metodo(int n) {
+		if(n%2 != 0) {
+			return 1;
+		}else {
+			return metodo (n/2) - 1;
+		}
+	}
+	
+	
+
+}
